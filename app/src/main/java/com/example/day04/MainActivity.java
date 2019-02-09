792f80f6e08a7570c3956e549ae965bba834ba8e
Jamie <jamielife1101@gmail.com>
2019-02-09 11:16:36 +0900

    editText 내용 주고 받기 성공

--- app/src/main/java/com/example/day04/MainActivity.java
+++ app/src/main/java/com/example/day04/MainActivity.java
@@ -5,29 +5,50 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout cont;
     LayoutInflater inflater;
+    TextView tv3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+
         cont = findViewById(R.id.container);
+        tv3 = findViewById(R.id.textView3);
 
         inflater.inflate(R.layout.sub1, cont, true);// 시작할 때부터 sub1으로 시작하고 싶을때.
     }
 
     public void xmlChange1(View v) {
+        EditText et = cont.findViewById(R.id.editText);
+        String str = et.getText().toString();
+
         cont.removeAllViews();
         inflater.inflate(R.layout.sub1, cont, true);
+
+        tv3.setText("현재: sub1");
+
+        et = cont.findViewById(R.id.editText);
+        et.setText(str);
     }
 
     public void xmlChange2(View v) {
+        EditText et = cont.findViewById(R.id.editText);
+        String str = et.getText().toString();
+
         cont.removeAllViews();
         inflater.inflate(R.layout.sub2, cont, true);
+
+        tv3.setText("현재: sub2");
+
+        et = cont.findViewById(R.id.editText);
+        et.setText(str);
     }
 }
