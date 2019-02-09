b109b0dc88c9426290891261c2e9afdbe5cba87f
Jamie <jamielife1101@gmail.com>
2019-02-09 15:22:38 +0900

    EditText에 쓴 내용 새로운 액티비티에 보내기 새로운 액티비티에서 finish()해서 Main으로 돌아올때 데이터 가져오기 위해서 새로운 액티비티 시작할때 그냥 startActivity(Intent) 메소드가 아니라 startActivityForResult 메소드를 써야하는 점 유의하기. 그런다음에 새로운 액티비티에서 받은 자료를 onActivityResult에서 처리해야함.

--- intent/src/main/java/com/example/intent/Abcd.java
+++ intent/src/main/java/com/example/intent/Abcd.java
@@ -1,32 +1,42 @@
 package com.example.intent;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
+import android.support.v13.view.inputmethod.EditorInfoCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
 
 public class Abcd extends AppCompatActivity {
 
+    EditText editText2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_abcd);
+        editText2 = findViewById(R.id.editText2);
+
         Intent main = getIntent();
+//        int data = main.getIntExtra("num",0);
+//        Log.i("num", data+"");
+//        editText2.setText(data+"");
 
-        int data = main.getIntExtra("num",0);
-
-        Log.i("num", data+"");
+        editText2.setText(main.getStringExtra("editText1"));
     }
 
     public void goToMain(View v) {
 
-        Intent main = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(main);
+//        Intent main = new Intent(getApplicationContext(), MainActivity.class);
+//        startActivity(main);
+//        startActivityForResult(main, 0001);
     }
 
     public void finish(View v) {
-
+        Intent intent = new Intent();
+        intent.putExtra("editText2",editText2.getText().toString());
+        setResult(RESULT_OK, intent);
         this.finish();
     }
 }
--- intent/src/main/java/com/example/intent/MainActivity.java
+++ intent/src/main/java/com/example/intent/MainActivity.java
@@ -1,25 +1,38 @@
 package com.example.intent;
 
 import android.content.Intent;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
     }
 
     public void goToAbcd(View v) {
 
         Intent abcd = new Intent(getApplicationContext(), Abcd.class);
 
-        abcd.putExtra("num", 1234);
+        abcd.putExtra("editText1", editText.getText().toString());
 
-        startActivity(abcd);
+//        startActivity(abcd);
+        startActivityForResult(abcd, 0002);
+    }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (resultCode == RESULT_OK) {
+            editText.setText(data.getStringExtra("editText2"));
+        }
     }
 }
