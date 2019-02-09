0070f99131896f2d5f8085ee29d4a3fb69725cfa
Jamie <jamielife1101@gmail.com>
2019-02-09 12:33:16 +0900

    버튼 클릭시 해당 액티비티를 인텐트로 보여줌.

--- intent/src/main/java/com/example/intent/Abcd.java
+++ intent/src/main/java/com/example/intent/Abcd.java
@@ -5,18 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+public class Abcd extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_abcd);
     }
 
-    public void goToAbcd(View v) {
+    public void goToMain(View v) {
 
-        Intent abcd = new Intent(getApplicationContext(), Abcd.class);
-        startActivity(abcd);
-
+        Intent main = new Intent(getApplicationContext(), MainActivity.class);
+        startActivity(main);
     }
 }
