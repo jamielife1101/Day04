cb9496a5e46a3e06674d9e13a9b02a6bfe2bb564
Jamie <jamielife1101@gmail.com>
2019-02-09 19:06:55 +0900

    PDF 보여주기 성공

--- pdfviewer/src/main/java/com/example/pdfviewer/MainActivity.java
+++ pdfviewer/src/main/java/com/example/pdfviewer/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.pdfviewer;
 
+import android.Manifest;
 import android.content.Intent;
+import android.net.Uri;
+import android.os.Environment;
+import android.os.StrictMode;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+
+import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,11 +18,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ActivityCompat.requestPermissions(this,
+                new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                1);
+        StrictMode.VmPolicy.Builder builder = new StrictMode.VmPolicy.Builder();
+        StrictMode.setVmPolicy(builder.build());
     }
 
     public void pdfOpen(View v) {
+        String  sdCardPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+        File file = new File(sdCardPath+"/ADSP요약정리.pdf");
+        Uri uri = Uri.fromFile(file);
+
         Intent intent = new Intent(Intent.ACTION_VIEW);
 
+        intent.setDataAndType(uri, "application/pdf");
+//        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         startActivity(intent);
     }
 }
