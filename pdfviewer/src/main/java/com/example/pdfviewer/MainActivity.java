60bc8d4c28f675203e565479bbd82d654920245d
Jamie <jamielife1101@gmail.com>
2019-02-10 17:39:19 +0900

    device 안에 있는 사진 불러오는 작업.
    선생님 말씀대로 했는데 안됨........

--- pdfviewer/src/main/java/com/example/pdfviewer/MainActivity.java
+++ pdfviewer/src/main/java/com/example/pdfviewer/MainActivity.java
@@ -9,15 +9,19 @@ import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import java.io.File;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText editText;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        editText = findViewById(R.id.editText);
 
         ActivityCompat.requestPermissions(this,
                 new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
@@ -35,6 +39,20 @@ public class MainActivity extends AppCompatActivity {
 
         intent.setDataAndType(uri, "application/pdf");
 //        intent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(intent);
+    }
+
+    public void imgOpen(View v) {
+        String  sdCardPath = Environment.getExternalStorageDirectory().getAbsolutePath();
+//        String  sdCardPath = Environment.getExternalStorageDirectory().toString();
+        Toast.makeText(getApplicationContext(), sdCardPath, Toast.LENGTH_SHORT).show();
+        File file = new File(sdCardPath+"/Cats/"+editText.toString()+".jpg");
+        Uri uri = Uri.fromFile(file);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+
+        intent.setDataAndType(uri, "image/*");
+//        intent.setFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
         startActivity(intent);
     }
 }
